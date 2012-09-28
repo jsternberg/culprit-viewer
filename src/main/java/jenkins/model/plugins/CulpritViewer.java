5a7ef1486366b5461d115d7407184124b4d18fc5
Jonathan Sternberg <jonathan.sternberg@gmail.com>
2012-09-27 22:07:51 -0400

    Culprit view shows all users in the culprits list.
    It also supports refreshing the list of culprits. It removes the
    cached set of culprits and then caches the result of "getCulprits"
    again.

---
+++ src/main/java/jenkins/model/plugins/CulpritViewer.java
@@ -0,0 +1,79 @@
+package jenkins.model.plugins;
+
+import com.google.common.collect.ImmutableSortedSet;
+import hudson.Extension;
+import hudson.model.AbstractBuild;
+import hudson.model.Action;
+import hudson.model.Result;
+import hudson.model.TransientBuildActionFactory;
+import hudson.model.User;
+
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+
+import java.lang.reflect.Field;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import javax.servlet.ServletException;
+
+public class CulpritViewer implements Action {
+
+    private transient AbstractBuild<?,?> build;
+
+    public CulpritViewer(AbstractBuild<?,?> build) {
+        this.build = build;
+    }
+
+    public String getIconFileName() {
+        return "user.png";
+    }
+
+    public String getDisplayName() {
+        return "Culprit Viewer";
+    }
+
+    public String getUrlName() {
+        return "culpritViewer";
+    }
+
+    public AbstractBuild getBuild() {
+        return build;
+    }
+
+    public Set<User> getCulprits() {
+        return build.getCulprits();
+    }
+
+    public void doRefresh(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
+        try {
+            Field culpritsField = AbstractBuild.class.getDeclaredField("culprits");
+            culpritsField.setAccessible(true);
+            culpritsField.set(build, null);
+
+            HashSet<String> r = new HashSet<String>();
+            for (User u : build.getCulprits())
+                r.add(u.getId());
+            culpritsField.set(build, ImmutableSortedSet.copyOf(r));
+            build.save();
+        } catch (NoSuchFieldException e) {
+            // The private interface changed from under us... whoops
+        } catch (IllegalAccessException e) {
+            // Should not hit this since we've made the attribute accessible
+        }
+        rsp.sendRedirect(".");
+    }
+
+    @Extension
+    public static final class DescriptorImpl extends TransientBuildActionFactory {
+        public Collection<? extends Action> createFor(AbstractBuild build) {
+            Result r = build.getResult();
+            if (r != null && r.isWorseThan(Result.SUCCESS))
+                return Collections.singletonList(new CulpritViewer(build));
+            else
+                return Collections.emptyList();
+        }
+    }
+}
